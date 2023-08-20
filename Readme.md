09c532f8be0d72b1bec8be1e6681cf93c9f5a239
megaconfidence <cokoghenun@gmail.com>
2023-08-21 00:42:08 +0100

    feat: Adds email service

---
+++ Readme.md
@@ -0,0 +1,5 @@
+open SSL
+For example, `libssl-dev` on Ubuntu or `openssl-devel` on Fedora. 
+
+
+headless chrome
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
 use std::fs;
+use reqwest;
 use anyhow::Result;
+use serde_json::json;
 use headless_chrome::Browser;
+use base64::{Engine as _, engine::general_purpose};
+
+#[macro_use]
+extern crate dotenv_codegen;
+
+async fn send_email(data:&Vec<u8>) -> Result<reqwest::Response, reqwest::Error>{
+    let base64Data =  general_purpose::STANDARD.encode(data);
+
+    let body = json!({
+        "From": "confidence@confidence.sh",
+        "To": "cokoghenun@gmail.com",
+        "Subject": "json",
+        "TextBody": "json",
+        "Attachments": [
+            {
+                "Name": "web.pdf",
+                "Content": base64Data,
+                "ContentType": "application/pdf"
+            }
+        ]
+    });
 
 
-async fn send_email() {
+
+    let client = reqwest::Client::new();
+    let res = client.post("https://api.postmarkapp.com/email")
+        .header("Content-Type", "application/json")
+        .header("X-Postmark-Server-Token", dotenv!("POSTMARK_TOKEN"))
+        .json(&body)
+        .send()
+        .await;
+    
+    return res;
 }
 
 #[tokio::main]
@@ -12,7 +44,7 @@ async fn main() -> Result<()>{
     let tab = browser.new_tab()?;
 
     let wikidata = tab 
-        .navigate_to("http://example.com/")?
+        .navigate_to("http://youtube.com/")?
         .wait_until_navigated()?
         .print_to_pdf(None)?;
 
@@ -20,10 +52,10 @@ async fn main() -> Result<()>{
 
     // println!("{:?}", wikidata);
     // println!("{}", std::str::from_utf8(&wikidata).unwrap() );
-    fs::write("wiki.pdf", wikidata)?;
+    fs::write("wiki.pdf", &wikidata)?;
     println!("PDF successfully created from internet web page.");
 
-    let res = send_email().await;  
+    let res = send_email(&wikidata).await;  
     println!("{:?}", res);
 
     Ok(())
