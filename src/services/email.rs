828df8db9a57c60b5841935efe1e425ff097f8c2
megaconfidence <cokoghenun@gmail.com>
2023-08-27 11:52:53 +0100

    feat: Moves email sevice to smtp

--- src/services/email.rs
+++ src/services/email.rs
@@ -1,52 +1,30 @@
 extern crate slug;
 use slug::slugify;
 
-use anyhow::Result;
-use base64::{Engine as _, engine::general_purpose};
+// use anyhow::Result;
+use mail_send::{SmtpClientBuilder};
+use mail_builder::MessageBuilder;
 
-use lettre::message::{Mailbox, header::ContentType, Attachment, MultiPart, SinglePart};
-use lettre::Address;
-use lettre::transport::smtp::authentication::Credentials;
-use lettre::{Message, SmtpTransport, Transport};
-
-pub async fn send_email(data:&Vec<u8>, to_email: &String, url: &String) -> Result<()> {
-    let base64_data =  general_purpose::STANDARD.encode(data);
-    let smtp_from_email = std::env::var("SMTP_FROM_EMAIL").expect("provide $SMTP_FROM_EMAIL");
+pub async fn send_email(data:&Vec<u8>, to_email: &String, url: &String) -> Result<(), mail_send::Error> {
+    let file_name = slugify(url)+".pdf";
+    let smtp_server = std::env::var("SMTP_SERVER").expect("provide $SMTP_SERVER");
     let smtp_username = std::env::var("SMTP_USERNAME").expect("provide $SMTP_USERNAME");
     let smtp_password = std::env::var("SMTP_PASSWORD").expect("provide $SMTP_PASSWORD");
-    let smtp_server = std::env::var("SMTP_SERVER").expect("provide $SMTP_SERVER");
-    let file_name = slugify(url)+".pdf";
+    let smtp_from_email = std::env::var("SMTP_FROM_EMAIL").expect("provide $SMTP_FROM_EMAIL");
+    let smtp_port = std::env::var("SMTP_PORT").expect("provide $SMTP_PORT").parse().unwrap();
     
-    println!("1");
-    let email = Message::builder()
-        .from(Mailbox::new(None, smtp_from_email.parse::<Address>()?))
-        .to(Mailbox::new(None, to_email.parse::<Address>()?))
+    let message = MessageBuilder::new()
+        .from(smtp_from_email)
+        .to(to_email.to_string())
         .subject("New Webpage Order Just Got Delivered!")
-        .header(ContentType::TEXT_PLAIN)
-        // .multipart(
-        //     MultiPart::mixed()
-        //         .singlepart(SinglePart::plain(String::from("Hey there, here's your freshly baked webpage from send2kindle. Enjoy!")))
-        //         // .singlepart(Attachment::new(file_name).body(base64_data,  ContentType::parse("application/pdf").unwrap()))
-        // )
-        .body(String::from("Hey there, here's your freshly baked webpage from send2kindle. Enjoy!"))
-        .unwrap();
+        .text_body("Hey there, here's your freshly baked webpage from send2kindle. Enjoy!")
+        .attachment("application/pdf", file_name, data.to_owned());
 
-    println!("2");
-    let creds = Credentials::new(smtp_username, smtp_password);
-
-    println!("3");
-    let mailer = SmtpTransport::relay(&smtp_server)
+    return SmtpClientBuilder::new(smtp_server, smtp_port)
+        .implicit_tls(false)
+        .credentials((smtp_username, smtp_password))
+        .connect()
+        .await
         .unwrap()
-        .credentials(creds)
-        .port(25)
-        .build();
-
-    println!("4");
-    match mailer.send(&email) {
-        Ok(_) => println!("Email sent successfully!"),
-        Err(e) => panic!("Could not send email: {:?}", e),
-    }
-
-    println!("5");
-    Ok(())
+        .send(message).await;
 }
