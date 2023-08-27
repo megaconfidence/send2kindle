5b0b6a453b323f4b2ac4eb680dcc19255de6baba
megaconfidence <cokoghenun@gmail.com>
2023-08-27 09:17:33 +0100

    feat: Adds smtp with lettre

--- src/services/email.rs
+++ src/services/email.rs
@@ -1,39 +1,52 @@
 extern crate slug;
 use slug::slugify;
 
-use reqwest;
 use anyhow::Result;
-use serde_json::json;
 use base64::{Engine as _, engine::general_purpose};
 
-pub async fn send_email(data:&Vec<u8>, email: &String, url: &String) -> Result<reqwest::Response, reqwest::Error>{
+use lettre::message::{Mailbox, header::ContentType, Attachment, MultiPart, SinglePart};
+use lettre::Address;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+
+pub async fn send_email(data:&Vec<u8>, to_email: &String, url: &String) -> Result<()> {
     let base64_data =  general_purpose::STANDARD.encode(data);
-    let from_email = std::env::var("FROM_EMAIL").expect("provide $FROM_EMAIL");
-    let postmark_token = std::env::var("POSTMARK_TOKEN").expect("provide $POSTMARK_TOKEN");
+    let smtp_from_email = std::env::var("SMTP_FROM_EMAIL").expect("provide $SMTP_FROM_EMAIL");
+    let smtp_username = std::env::var("SMTP_USERNAME").expect("provide $SMTP_USERNAME");
+    let smtp_password = std::env::var("SMTP_PASSWORD").expect("provide $SMTP_PASSWORD");
+    let smtp_server = std::env::var("SMTP_SERVER").expect("provide $SMTP_SERVER");
+    let file_name = slugify(url)+".pdf";
+    
+    println!("1");
+    let email = Message::builder()
+        .from(Mailbox::new(None, smtp_from_email.parse::<Address>()?))
+        .to(Mailbox::new(None, to_email.parse::<Address>()?))
+        .subject("New Webpage Order Just Got Delivered!")
+        .header(ContentType::TEXT_PLAIN)
+        // .multipart(
+        //     MultiPart::mixed()
+        //         .singlepart(SinglePart::plain(String::from("Hey there, here's your freshly baked webpage from send2kindle. Enjoy!")))
+        //         // .singlepart(Attachment::new(file_name).body(base64_data,  ContentType::parse("application/pdf").unwrap()))
+        // )
+        .body(String::from("Hey there, here's your freshly baked webpage from send2kindle. Enjoy!"))
+        .unwrap();
 
-    let body = json!({
-    "From": from_email,
-    "To": email,
-    "Subject": "New Webpage Order Just Got Delivered!",
-    "TextBody": "Hey there, here's your freshly baked webpage from send2kindle. Enjoy!",
-    "Attachments": [
-    {
-    "Name": slugify(url)+".pdf",
-    "Content": base64_data,
-    "ContentType": "application/pdf"
+    println!("2");
+    let creds = Credentials::new(smtp_username, smtp_password);
+
+    println!("3");
+    let mailer = SmtpTransport::relay(&smtp_server)
+        .unwrap()
+        .credentials(creds)
+        .port(25)
+        .build();
+
+    println!("4");
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => panic!("Could not send email: {:?}", e),
     }
-    ]
-    });
 
-
-
-    let client = reqwest::Client::new();
-    let res = client.post("https://api.postmarkapp.com/email")
-        .header("Content-Type", "application/json")
-        .header("X-Postmark-Server-Token", postmark_token)
-        .json(&body)
-        .send()
-    .await;
-
-    return res;
+    println!("5");
+    Ok(())
 }
