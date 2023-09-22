2743a73ae2c72c6f357f89dd8124e398011f38ce
megaconfidence <cokoghenun@gmail.com>
2023-09-22 10:43:43 +0100

    feat: Adds file compression

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use regex::Regex;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+use walkdir::WalkDir;
+
+pub fn compress_pdf(in_path: &Path) -> Vec<u8> {
+    let in_path_str = in_path.to_string_lossy().to_string();
+    let out_path_str = format!("compressed_{}", in_path_str);
+    let out_path = Path::new(&out_path_str);
+
+    Command::new("gs")
+        .args(
+            [
+                "-dQUIET",
+                "-dBATCH",
+                "-dNOPAUSE",
+                "-sDEVICE=pdfwrite",
+                "-dCompatibilityLevel=1.4",
+                "-dPDFSETTINGS=/ebook",
+            ]
+            .iter(),
+        )
+        .arg(format!("-sOutputFile={}", out_path_str))
+        .arg(in_path_str)
+        .output()
+        .expect("failed to compress pdf");
+
+    fs::read(out_path).expect("error reading output pdf")
+}
+
+pub fn clean_files(file_id: &String) -> Result<()> {
+    let re = Regex::new(file_id).unwrap();
+    for entry in WalkDir::new(".") {
+        let entry = entry?;
+        if re.is_match(entry.path().to_str().unwrap()) {
+            fs::remove_file(entry.path())?;
+            // println!("{}", entry.path().display());
+        }
+    }
+    Ok(())
+}
--- src/services/pdf.rs
+++ src/services/pdf.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 use chromiumoxide::browser::{Browser, BrowserConfig};
 use chromiumoxide::cdp::browser_protocol::page::PrintToPdfParams;
 use futures::StreamExt;
+use nanoid::nanoid;
+use std::path::Path;
 
 pub async fn gen_pdf(url: &String) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let pdf_options = PrintToPdfParams {
@@ -36,13 +38,17 @@ pub async fn gen_pdf(url: &String) -> Result<Vec<u8>, Box<dyn std::error::Error>
     page.evaluate(scroll_js).await?;
     page.evaluate(image_js).await?;
 
-    let pdf = page.pdf(pdf_options).await?;
-    // let pdf = page
-    //     .save_pdf(pdf_options, std::path::Path::new("./web.pdf"))
-    //     .await?;
+    let job_id = nanoid!();
+    let file_name = format!("webpage_{}_.pdf", job_id);
+    let pdf_path = Path::new(&file_name);
+
+    page.save_pdf(pdf_options, pdf_path).await?;
+    let pdf = send2kindle::compress_pdf(pdf_path);
+    send2kindle::clean_files(&job_id)?;
 
     browser.close().await?;
     handle.await?;
 
     Ok(pdf)
 }
+
